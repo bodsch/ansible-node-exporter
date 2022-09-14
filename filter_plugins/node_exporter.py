fd5ecd9b02c2a66bb36e4178a44fc7aeb3f4977f
Bo.Sch <bodo@boone-schulz.de>
2022-09-14 11:56:29 +0200

    Feature/gh 2 detect and install latest version (#3)
    * enable direct downloads
    * detect and install latest version
    * update build environment

---
+++ filter_plugins/node_exporter.py
@@ -0,0 +1,41 @@
+# python 3 headers, required if submitting to Ansible
+
+from __future__ import (absolute_import, print_function)
+__metaclass__ = type
+
+import re
+from ansible.utils.display import Display
+
+display = Display()
+
+
+class FilterModule(object):
+    """
+        Ansible file jinja2 tests
+    """
+    def filters(self):
+        return {
+            'type': self.var_type,
+            'node_exporter_checksum': self.checksum,
+        }
+
+    def var_type(self, var):
+        """
+        """
+        return type(var).__name__
+
+    def checksum(self, data, os, arch):
+        """
+        """
+        checksum = None
+
+        if isinstance(data, list):
+            # filter OS
+            # linux = [x for x in data if re.search(r".*node_exporter-.*.{}.*.tar.gz".format(os), x)]
+            # filter OS and ARCH
+            checksum = [x for x in data if re.search(r".*node_exporter-.*.{}-{}.tar.gz".format(os, arch), x)][0]
+
+        if isinstance(checksum, str):
+            checksum = checksum.split(" ")[0]
+        # display.v("= checksum: {}".format(checksum))
+        return checksum
